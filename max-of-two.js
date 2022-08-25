6f1447c170313f1fc7709f3b50367733102087e8
Christopher langley <christopherlangley@ChristophersMBP.lan>
2022-08-24 20:34:51 -0700

    MO2

---
+++ max-of-two.js
@@ -0,0 +1,18 @@
+let x = 1;
+let y = 2;
+ if ( y > x){
+    console.log("THe bigger value is 2");
+ }
+ else{
+    console.log(" the bigger value is 1");
+ }
+
+ x = 47
+ y = 47
+
+ if (x == y ) {
+    console.log("The values are identical");
+ }
+ else{
+    console.log("math is broken");
+ }
